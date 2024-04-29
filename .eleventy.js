b8e5492151bee2c3b3d95312a3cb9dd14be0acfa
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-04-29 16:29:12 -0400

    adds components, default layout and partials

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassThroughCopy(src / css);
+
+  eleventyConfig.addShortCode('component_dsd', function (name) {
+    const fileContents = fs.readFileSync(
+      `src/_includes/components/${name}.html`
+    );
+    return fileContents;
+  });
+};
+
 return {
   dir: {
     input: 'src',
